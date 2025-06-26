47a752399304122fea5f050da8d7d95342da8b96
regiisan <regiisanchez@hotmail.com>
2025-06-26 13:49:14 -0300

    Config hibernate

--- src/main/java/com/tallerwebi/config/HibernateConfig.java
+++ src/main/java/com/tallerwebi/config/HibernateConfig.java
@@ -16,7 +16,7 @@ public class HibernateConfig {
 
     @Bean
     public DataSource dataSource() {
-
+        /*
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
         dataSource.setUrl("jdbc:hsqldb:mem:db_");
@@ -26,8 +26,8 @@ public class HibernateConfig {
         //dataSource.setUrl("jdbc:mysql://mi_mysql:3306/tallerwebi");
         //dataSource.setUsername("user");
         //dataSource.setPassword("user");
-        return dataSource;
-/*
+        return dataSource;*/
+
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
         String dbHost = System.getenv("DB_HOST");
@@ -49,7 +49,7 @@ public class HibernateConfig {
         dataSource.setUsername(dbUser);
         dataSource.setPassword(dbPassword);
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        return dataSource;*/
+        return dataSource;
     }
 
     @Bean
@@ -68,8 +68,8 @@ public class HibernateConfig {
 
     private Properties hibernateProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
-        //properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL57Dialect");
+        //properties.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL57Dialect");
         properties.setProperty("hibernate.show_sql", "true");
         properties.setProperty("hibernate.format_sql", "true");
         properties.setProperty("hibernate.hbm2ddl.auto", "create");
